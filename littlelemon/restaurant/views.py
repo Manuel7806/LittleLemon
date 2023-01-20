f90a977bc4ccb582e11c5df37dd9a59c5b2401a0
Manuel De La Rosa <manueldelarosa9722@gmail.com>
2023-01-20 16:29:56 -0600

    Added API endpoints

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleItemView(RetrieveAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
